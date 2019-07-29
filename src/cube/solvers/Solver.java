2a41c56ebc7d989daa2cfc0f96d82e8c19c44ae6
TheNightly <jierui1998@gmail.com>
2019-07-29 15:43:41 -0700

    Deleted metadata and added solver interface

---
+++ src/cube/solvers/Solver.java
@@ -0,0 +1,18 @@
+package cube.solvers;
+
+import cube.*;
+
+public interface Solver {
+	/**
+	 * Returns as a string the type of solver 
+	 * @return
+	 */
+	public String getType();
+	
+	/**
+	 * Uses the current solver method to give the best action from the current state. 
+	 * @return
+	 */
+	public Action getBestAction();
+	
+}
