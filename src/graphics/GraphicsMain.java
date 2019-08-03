da9d2ae5a34a3293300b29c6ae8fd74e58aa2085
TheNightly <jierui1998@gmail.com>
2019-08-02 23:02:51 -0700

    Fixed JFrame dimensions for 10 cube example.

--- src/graphics/GraphicsMain.java
+++ src/graphics/GraphicsMain.java
@@ -13,11 +13,11 @@ import javax.swing.JPanel;
 public class GraphicsMain {	
 	public static void main(String[] args) {
 		JFrame mainDisplay = new JFrame();
-		mainDisplay.setSize(1440, 720);
+		mainDisplay.setSize(1518, 600);
 		int windowSize = 300;
 		JPanel container = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0)); 
 		//container.setLayout(new BoxLayout(container, FlowLayout));
-		for(int i = 1; i < 10; i++) {
+		for(int i = 1; i < 11; i++) {
 			Scene3D s = new Scene3D(i, true, windowSize);
 			s.setPreferredSize(new Dimension(windowSize, windowSize));
 			container.add(s);
